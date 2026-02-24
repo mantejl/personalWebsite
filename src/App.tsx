6d6c6b44ac10e06a440412caae71c8bc1a1577c7
Mantej Lamba <mlamba@usc.edu>
2026-02-24 00:45:19 -0800

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "@/components/Navbar";
 import Index from "./pages/Index";
 import Experience from "./pages/Experience";
@@ -29,6 +30,7 @@ const App = () => (
             <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
+          <Analytics />
         </div>
       </BrowserRouter>
     </TooltipProvider>
